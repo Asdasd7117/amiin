9dbc24f57bb645c542ced8b52bf533ddab21dfaa
Asdasd7117 <alywm100@gmail.com>
2026-06-30 02:52:32 +0300

    Update navigation-compose dependency to version 2.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.google.android.material:material:1.12.0")
 
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.navigation:navigation-compose:2.8.0")
 
     implementation("androidx.datastore:datastore-preferences:1.1.1")
 
